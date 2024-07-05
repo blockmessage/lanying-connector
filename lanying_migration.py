f6e13f4552c408dc42fc6e837fa6c4314e914c8e
ZouJinHai <jinhai@bmxlabs.com>
2024-07-05 16:25:02 +0800

    Add migration old article add keywords and description

--- lanying_migration.py
+++ lanying_migration.py
@@ -7,6 +7,7 @@ from lanying_grow_ai import GitBookSummary
 import os
 import re
 import shutil
+import requests
 
 def info(format):
     print(format)
@@ -98,3 +99,107 @@ def transform_summary_remove_date_directory(base_dir):
     with open(summary_file, 'w') as f:
         f.write(gitbook_summary.to_markdown())
     return gitbook_summary
+
+def add_page_keywords_and_description(directory, max_process_count = 5):
+    process_queue = []
+    article_id = 0
+    for root, dirs, files in os.walk(directory):
+        if article_id >= max_process_count:
+            break
+        for file in files:
+            if file.endswith(".md") and file not in ['SUMMARY.md']:
+                full_file = os.path.join(root, file)
+                with open(full_file) as f:
+                    content = f.read()
+                    lines = content.splitlines()
+                    if len(lines) <= 2:
+                        print(f'skip for line too less: {full_file}')
+                        continue
+                    if re.search(f'^description: .*', content, re.MULTILINE):
+                        print(f'skip for description exist: {full_file}')
+                        continue
+                    match = re.search(r'^(#|title:) (.*)', content, re.MULTILINE)
+                    if match:
+                        title = match.group(2).strip('" ')
+                    else:
+                        title = ''
+                    summary = extract_summary(content)
+                    article_id += 1
+                    process_info = {'file': file, 'full_file': full_file, 'content': content, 'summary': summary, 'article_id': article_id, 'title': title}
+                    process_queue.append(process_info)
+                    if article_id >= max_process_count:
+                        break
+                    if len(process_queue) >= 10:
+                        process_queue_page_keywords_and_description(process_queue)
+                        process_queue = []
+    if len(process_queue) > 0:
+        process_queue_page_keywords_and_description(process_queue)
+
+def process_queue_page_keywords_and_description(process_queue):
+    article_infos = []
+    for process_info in process_queue:
+        article_infos.append({'article_id': process_info['article_id'],'content': process_info['summary']})
+    article_infos
+    prompt_lines = ['我会给你多个网页的ID和主要内容，请根据输入的网页ID和网页主要内容， 生成每个网页的元数据。请直接返回JSON格式的列表，前后不要有额外内容。',
+              '元数据包括：网页ID，网页的描述，网页的搜索引擎SEO关键词，网页的搜索引擎SEO额外关键词。',
+              '网页的描述: 用于告知搜索引擎SEO优化的网页描述，默认使用中文，请根据我提供的网页主要内容来总结，150个字符以内，单段不换行，请保持语句通顺完整，结尾有标点表示句子结束。',
+              '网页的搜索引擎SEO关键词: 2 个关键词, 字符串类型，多个关键词请以英文逗号和空格连接。',
+              '网页的搜索引擎SEO额外关键词: 请从提供的可能的关键词列表中选出 2 个最适合作为本文关键词且不在[网页搜索引擎SEO关键词]里的关键词，可能的关键词列表为：IM SDK,即时通讯SDK,APP内聊天功能,IM开源,IM云服务,PUSH SDK,第三方推送,RTC SDK,实时音视频,Chat AI SDK,企业级AI,AI Agent,AI智能体。',
+              '输出格式为:[{"article_id": 1, "description": "", "keywords": "", "extra_keywords":""},{"article_id": 2, "description": "", "keywords": "", "extra_keywords":""}, ...]',
+              f'网页主要内容列表为：{json.dumps(article_infos, ensure_ascii=False)}']
+    prompt = "\n".join(prompt_lines)
+    # print(f'prompt: {prompt}\n')
+    url = 'https://connector-preview.lanyingim.com/v1/chat/completions'
+    api_key = os.getenv('openapi_key')
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
+    body = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        "max_tokens": 4096
+    }
+    print("request to openai")
+    response = requests.request("POST", url, headers=headers, json=body)
+    print(f'respose: {response.text}')
+    res = response.json()
+    response_message = res['choices'][0]['message']
+    reply = response_message.get('content', "")
+    metadata_list = json.loads(reply)
+    for metadata in metadata_list:
+        article_id = metadata.get('article_id',0)
+        description = metadata.get('description', '')
+        keywords = metadata.get('keywords', '')
+        extra_keywords = metadata.get('extra_keywords', '')
+        if not(article_id > 0 and description != '' and keywords != '' and extra_keywords != ''):
+            print(f"bad metadata: {metadata}")
+            continue
+        process_info = next((item for item in process_queue if item["article_id"] == article_id), None)
+        if process_info is None:
+            print(f'bad article_id:{metadata}')
+            continue
+        full_file = process_info['full_file']
+        content = process_info['content']
+        title = process_info['title']
+        markdown_header = f'---\ndescription: {description}\nkeywords: {keywords}, {extra_keywords}\n---\n'
+        print(f'update file:{full_file}\ntitle:{title}\nheader:{markdown_header}')
+        with open(full_file, 'w') as f:
+            f.write(f'{markdown_header}{content}')
+
+def extract_summary(content):
+    lines = []
+    # 提取1、2、3级标题
+    headers = re.findall(r'^#{1,3}\s+(.*)$', content, flags=re.MULTILINE)
+    count = 0
+    max_count = 500
+    for header in headers:
+        header = header.strip()
+        count += len(header)
+        if count <= max_count or len(lines) == 0:
+            lines.append(header)
+    if len(lines) < 2:
+        lines = [content[:300]]
+    return '\n'.join(lines)
